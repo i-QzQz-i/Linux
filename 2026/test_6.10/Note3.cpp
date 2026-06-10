ed9ef5c09d8d73477aaa325b7fe96077b0f62869
191599805 <191599805@qq.com>
2026-06-10 20:55:21 +0800

    Linux基础学习 4

---
+++ 2026/test_6.10/Note3.cpp
@@ -0,0 +1,67 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+// 
+// 是什么
+// 打包：文件合并
+// 压缩：减少体积
+// 
+// 为什么
+// 打包：防止文件丢失
+// 压缩：减少体积，节约空间；传输时节约时间
+// 
+// 体系结构（芯片架构）
+// x86_32, x86_64(x64)
+// 新老内核问题：用old
+// 
+// ‘ \ ’ 续行符
+// 重要热键
+// ctrl+c 
+// ↑↓
+// Tab     命令补全（按一下）与查询（按两下） 
+// ctrl+r  搜索历史命令（所以Linux会记录历史命令） 
+// ctrl+d  退出当前用户
+// 
+ 
+
+
+
+// 
+// yum install -y lrzsz
+// 
+// zip/unzip
+// zip
+// zip -r
+// 
+// unzip xxx.zip
+// unzip xxx.zip -d /
+// 
+// tar 
+// tar czf xxx.tar
+// 
+// tar xzf xxx.tar
+// tar xzf xxx.tar -C /
+// 
+// 
+// 传windos
+// sz/rz 
+// 
+// 传Linux
+// scp
+// 
+// bc
+// echo "1+2+3+4+5+6" | bc
+// 
+// uname
+// uname -a
+// uname -r
+// 
+// shutdown（云服务器不关机）
+// halt
+// reboot
+// 
+
+
+
+
+
+
