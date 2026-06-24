151e57722ae0f89b2df06a52782b7850aafeb60c
191599805 <191599805@qq.com>
2026-06-24 18:29:56 +0800

    Linux开发工具学习 6

---
+++ 2026/test_6.24/Note7 (开发工具)(make).cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+//
+//			make/makefile		  //
+// 是什么 
+// make是一个命令
+// makefile/Makefile是一个文件
+// 
+// makefile基本概念 --- 依赖关系，依赖方法
+// 
+// code:code.c                             （依赖关系：没有关系谁理你）
+//(打一个Tap) gcc -o code code.c           （依赖方法：你要干神魔）
+// 
+// 可执行程序放最前面
+// 
+// .PHONY:（忽略M时间）
+// 
+// 
+// 
+// stat --- "ACM"时间
+// 文件 == 
+// Access:
+// Modify:
+// Change:（特殊：不是时实更新）
+// 
+// 
+
+
+
+
+// 
+// stat --- "ACM"时间
+// 
+// man touch
+// touch
+// 
+// 
+//
