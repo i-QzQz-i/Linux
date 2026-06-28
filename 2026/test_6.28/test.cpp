4de3d46eab2212d1a23fc75aa95913651864a553
191599805 <191599805@qq.com>
2026-06-28 14:03:02 +0800

    make/Makefile基础实现 1

---
+++ 2026/test_6.28/test.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  	cout << "hi QzQz" << endl;
+
+//    cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+//  	cout << "hi QzQz" << endl;
+
+  	return 0;
+}
