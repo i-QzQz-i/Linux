522a855cd844d2d41c38329f4f4a3ec19ba03085
191599805 <191599805@qq.com>
2026-07-03 22:05:50 +0800

    Linux开发工具学习 8

---
+++ 2026/test_7.3/Note8 (开发工具)(git).cpp
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS 
+
+// 
+//		版本控制器git			
+// 
+//			Git			//
+//	一、如何理解版本控制
+// git是一个底层的版本控制器软件
+// gitee/github 是基于git的网站或平台
+// 
+// 去中心化，分布式的版本控制
+// 
+// 
+//	二、Git的历史
+// git是开源的
+// 
+//	三、Git的操作
+// 1.git提交时，只提交变化的部分
+// 2.首次使用：git输入username，email
+// 3.git版本管理，只进行管理源文件（.h、.c、.cpp）
+// 4. .gitignore：需要忽略的特定后缀的文件列表
+// 5.远端仓库，相比任何人，都是最新的（需要与远端仓库同步上）
+// 
+// 
+// git clone https:...
+// git status
+// git log
+// git add (.)
+// git commit -m "日志信息"
+// git push 
+// git pull
+// 
+// 
+//			提交代码
+// 
+//
